38e7827695f9ffed3aaf5b5f3f2aaf20888b59c5
unknown <apit2469@gmail.com>
2022-08-10 12:13:53 +0530

    table

---
+++ src/Components/Table/Table.js
@@ -0,0 +1,107 @@
+import * as React from "react";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import "./Table.css";
+
+const useSortableData = (
+  items,
+  config = { key: "id", direction: "ascending" }
+) => {
+  let direction;
+  const [sortConfig, setSortConfig] = React.useState(config);
+
+  const requestSort = (key) => {
+    direction = "ascending";
+    if (sortConfig && sortConfig.direction === "ascending") {
+      direction = "descending";
+    }
+    setSortConfig({ key, direction });
+  };
+
+  const sortedItems = React.useMemo(() => {
+    let sortableItems = [...items];
+    if (sortConfig !== null) {
+      sortableItems.sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === "ascending" ? -1 : 1;
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === "ascending" ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+
+    return sortableItems;
+  }, [items, sortConfig]);
+  return { items: sortedItems, requestSort };
+};
+
+export default function BasicTable(props) {
+  let { black, icons } = props;
+  const { items, requestSort } = useSortableData(props.data);
+
+  const renderTableHeader = () => {
+    let header = Object.keys(props.data[0]);
+    return header.map((key, index) => {
+      if (key === black) {
+        black = index;
+      }
+
+      return (
+        <TableCell align="center" key={index} className="TableHeadCell">
+          {
+            <div className="Cell">
+              {key.toUpperCase()}
+              {"  "}
+              {icons.includes(key.toLowerCase().trim()) ? null : (
+                <ArrowDropDownIcon onClick={() => requestSort(key)} />
+              )}
+            </div>
+          }
+        </TableCell>
+      );
+    });
+  };
+
+  return (
+    <div className="Table">
+      <TableContainer className="TableContainer">
+        <Table className="TableT" aria-label="simple table">
+          <TableHead className="TableHead">
+            <TableRow>{renderTableHeader()}</TableRow>
+          </TableHead>
+          <TableBody>
+            {items.map((item, id) => {
+              let col = Object.values(items[id]);
+              return (
+                <TableRow key={id}>
+                  {col.map((value, index) => {
+                    return (
+                      <TableCell
+                        align="left"
+                        key={index}
+                        className={
+                          index !== black
+                            ? "TableDataCell"
+                            : "TableDataCellBlack"
+                        }
+                      >
+                        {value}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+}
