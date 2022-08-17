caf1cfdf6070ac09c74f0a7007b0efb6f3a082f6
unknown <apit2469@gmail.com>
2022-08-17 14:02:07 +0530

    tabs done

--- src/Components/Tabs/Tabs.js
+++ src/Components/Tabs/Tabs.js
@@ -1,72 +1,97 @@
 import * as React from "react";
-import PropTypes from "prop-types";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
+import { useState } from "react";
+import "./Tabs.css";
 
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+import { students, students2, students4 } from "../../data";
+import BasicTable from "../Table/Table";
+
+import ArrowForwardIos from "@mui/icons-material/ArrowForwardIos";
+import Home from "@mui/icons-material/Home";
+import { Button } from "@mui/material";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
+function Tabs(props) {
+  const { tabs } = props;
+  const [currentTab, setCurrentTab] = useState("Fiscal Year");
+  console.log(currentTab);
+  const setTab = (item) => {
+    setCurrentTab(item);
+  };
+
+  const [action, setAction] = React.useState("");
+
+  const handleChange = (event) => {
+    setAction(event.target.value);
+  };
 
   return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
+    <div className="App">
+      <div className="Home-Bar">
+        <span className="Home">
+          <Home />{" "}
+          <ArrowForwardIos
+            style={{
+              fontSize: "15px",
+              marginLeft: "20px",
+              marginRight: "20px",
+            }}
+          />
+          Setup
+        </span>
+        <Button variant="contained" style={{ backgroundColor: "grey" }}>
+          Add New
+        </Button>
+      </div>
+      <div className="Tabs">
+        <div className="Tab">
+          {tabs.map((item, id) => (
+            <span
+              key={id}
+              id={currentTab === item ? `tabs-${id}` : null}
+              className="SingleTab"
+              onClick={() => setTab(item)}
+            >
+              {item}
+            </span>
+          ))}
+        </div>
+
+        <Box sx={{ minWidth: 120 }}>
+          <FormControl sx={{ minWidth: 120 }} size="small">
+            <InputLabel id="demo-select-small">Action</InputLabel>
+            <Select
+              labelId="demo-select-small"
+              id="demo-select-small"
+              value={action}
+              label="Action"
+              onChange={handleChange}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
         </Box>
+      </div>
+
+      {currentTab === "Fiscal Year" ? (
+        <BasicTable data={students} black="id" icons={["email"]} />
+      ) : currentTab === "Currency" ? (
+        <BasicTable data={students4} black="id" icons={["gg", "hh"]} />
+      ) : currentTab === "AR Setup" ? (
+        <BasicTable data={students2} black="id" icons={["name"]} />
+      ) : (
+        <BasicTable data={students2} black="name" icons={["id"]} />
       )}
     </div>
   );
 }
 
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-}
-
-export default function BasicTabs() {
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  return (
-    <Box sx={{ width: "100%" }}>
-      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          aria-label="basic tabs example"
-        >
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
-        </Tabs>
-      </Box>
-      <TabPanel value={value} index={0}>
-        Item One
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel>
-    </Box>
-  );
-}
+export default Tabs;
